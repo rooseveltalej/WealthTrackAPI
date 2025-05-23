3d524e2e0b65d19e52b78adbf7d016ae3ca7e2f4
akhoz <adrian25092005@gmail.com>
2025-05-22 19:31:54 -0600

    feat: added initial database script and fast api server

---
+++ db_scripts/db.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE wealth_track;
---
+++ db_scripts/tables.sql
@@ -0,0 +1,65 @@
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(50),
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Income (
+    date DATE NOT NULL,
+    userId INTEGER NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    PRIMARY KEY (date, userId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Expenses (
+    date DATE NOT NULL,
+    userId INTEGER NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    PRIMARY KEY (date, userId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Savings (
+    date DATE NOT NULL,
+    userId INTEGER NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    PRIMARY KEY (date, userId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Investments (
+    date DATE NOT NULL,
+    userId INTEGER NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    PRIMARY KEY (date, userId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE ExpenseGoals (
+    date DATE NOT NULL,
+    userId INTEGER NOT NULL,
+    value NUMERIC(5, 2) NOT NULL, -- porcentaje
+    PRIMARY KEY (date, userId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE SavingGoals (
+    date DATE NOT NULL,
+    userId INTEGER NOT NULL,
+    value NUMERIC(5, 2) NOT NULL,
+    PRIMARY KEY (date, userId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE InvestmentGoals (
+    date DATE NOT NULL,
+    userId INTEGER NOT NULL,
+    value NUMERIC(5, 2) NOT NULL,
+    PRIMARY KEY (date, userId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
+);
