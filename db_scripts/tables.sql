7c90db2c9f1604a4f95eaa60c61fbb12c8103c52
rooseveltalej <roperez@estudiantec.cr>
2025-06-06 14:47:02 -0600

    fix: update Expenses, Savings, and Investments tables to use a single primary key

--- db_scripts/tables.sql
+++ db_scripts/tables.sql
@@ -14,29 +14,29 @@ CREATE TABLE Income (
 );
 
 CREATE TABLE Expenses (
+    id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     userId INTEGER NOT NULL,
     amount NUMERIC(10, 2) NOT NULL,
     category VARCHAR(100) NOT NULL,
-    PRIMARY KEY (date, userId),
     FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Savings (
+    id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     userId INTEGER NOT NULL,
     amount NUMERIC(10, 2) NOT NULL,
     category VARCHAR(100) NOT NULL,
-    PRIMARY KEY (date, userId),
     FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Investments (
+    id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     userId INTEGER NOT NULL,
     amount NUMERIC(10, 2) NOT NULL,
     category VARCHAR(100) NOT NULL,
-    PRIMARY KEY (date, userId),
     FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
 );
 
